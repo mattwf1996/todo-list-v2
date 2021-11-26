879ce5d5b7843607c1b23a100d9d42cb0b584987
Ubuntu <mattfarrow@dev-jenkins.c33phsfnt12urk0dotv2or3jua.ax.internal.cloudapp.net>
2021-11-26 11:55:12 +0000

    deploy stage copies compose file to manager

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,4 +2,5 @@
 
 echo "Deploy stage"
 
+scp docker-compose.yaml jenkins@swarm-manager2:/home/jenkins/docker-compose.yaml
 ssh jenkins@swarm-manager2 docker stack deploy --compose-file docker-compose.yaml todo-app
