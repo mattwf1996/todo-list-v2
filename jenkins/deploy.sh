97048d5c38a2c42bcc4dc1254143bad72368a2a5
Ubuntu <mattfarrow@dev-jenkins.c33phsfnt12urk0dotv2or3jua.ax.internal.cloudapp.net>
2021-11-26 13:38:41 +0000

    fixing

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,4 +5,6 @@ echo "Deploy stage"
 scp docker-compose.yaml jenkins@swarm-manager2:/home/jenkins/docker-compose.yaml
 ssh jenkins@swarm-manager2 \
     DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+    DATABASE_URI=$DATABASE_URI \
+    CREATE_SCHEMA=true \
     docker stack deploy --compose-file docker-compose.yaml todo-app
