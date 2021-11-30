2617d06fa2bbd7fda616d040f25b6e96d274b575
Ubuntu <mattfarrow@dev-jenkins.c33phsfnt12urk0dotv2or3jua.ax.internal.cloudapp.net>
2021-11-30 16:23:23 +0000

    fixing test script to install pytest-cov 4

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -16,14 +16,14 @@ mkdir test_reports
 #  run pytest frontend
 python3 -m pytest frontend \
     --cov=frontend/application \
-    --cov-report term missing \
+    --cov-report term-missing \
     --cov-report xml:test_reports/frontend_coverage.xml \
     --junitxml=test_reports/frontend_junit_report.xml
 
 #  run pytest backend
 python3 -m pytest backend \
     --cov=backend/application \
-    --cov-report term missing \
+    --cov-report term-missing \
     --cov-report xml:test_reports/backend_coverage.xml \
     --junitxml=test_reports/backend_junit_report.xml
 
